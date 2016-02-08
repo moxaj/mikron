8f6646cd24292e1dd210fc0ddf659ed546b69774
viktor.magyari92 <viktor.magyari92@gmail.com>
2016-02-08 19:45:29 +0100

    Fix tabs for .java file.

--- src/java/seria/SeriaByteBuffer.java
+++ src/java/seria/SeriaByteBuffer.java
@@ -7,11 +7,11 @@ public class SeriaByteBuffer {
   private int bitPosition;
   private int maxByteLength;
 
-	private SeriaByteBuffer(int capacity) {
-		buffer = ByteBuffer.allocate(capacity);
-		bitPosition = 0;
+  private SeriaByteBuffer(int capacity) {
+    buffer = ByteBuffer.allocate(capacity);
+    bitPosition = 0;
     maxByteLength = 0;
-	}
+  }
 
   private SeriaByteBuffer(byte[] bytes) {
     buffer = ByteBuffer.wrap(bytes);
@@ -39,11 +39,11 @@ public class SeriaByteBuffer {
     return raw;
   }
 
-	////////////////////////////////////////////////////////////
-	//
-	// Positioning
-	//
-	////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
+  //
+  // Positioning
+  //
+  ////////////////////////////////////////////////////////////
 
   public int getBitPosition() {
     return bitPosition;
@@ -72,86 +72,86 @@ public class SeriaByteBuffer {
     return this;
   }
 
-	////////////////////////////////////////////////////////////
-	//
-	// Buffer methods
-	//
-	////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
+  //
+  // Buffer methods
+  //
+  ////////////////////////////////////////////////////////////
 
-	public boolean getBoolean() {
-		boolean value = (buffer.get(bitPosition / 8) & (1 << bitPosition % 8)) != 0;
-		bitPosition++;
-		return value;
-	}
+  public boolean getBoolean() {
+    boolean value = (buffer.get(bitPosition / 8) & (1 << bitPosition % 8)) != 0;
+    bitPosition++;
+    return value;
+  }
 
-	public SeriaByteBuffer putBoolean(boolean value) {
-		int position = bitPosition / 8;
-		byte b = buffer.get(position);
-		buffer.put(position, (byte) (value ? (b | (1 << bitPosition % 8)) : (b & ~(1 << bitPosition % 8))));
-		bitPosition++;
+  public SeriaByteBuffer putBoolean(boolean value) {
+    int position = bitPosition / 8;
+    byte b = buffer.get(position);
+    buffer.put(position, (byte) (value ? (b | (1 << bitPosition % 8)) : (b & ~(1 << bitPosition % 8))));
+    bitPosition++;
     return this;
-	}
+  }
 
-	public byte getByte() {
-		return buffer.get();
-	}
+  public byte getByte() {
+    return buffer.get();
+  }
 
-	public char getChar() {
-		return buffer.getChar();
-	}
+  public char getChar() {
+    return buffer.getChar();
+  }
 
-	public double getDouble() {
-		return buffer.getDouble();
-	}
+  public double getDouble() {
+    return buffer.getDouble();
+  }
 
-	public float getFloat() {
-		return buffer.getFloat();
-	}
+  public float getFloat() {
+    return buffer.getFloat();
+  }
 
-	public int getInt() {
-		return buffer.getInt();
-	}
+  public int getInt() {
+    return buffer.getInt();
+  }
 
-	public long getLong() {
-		return buffer.getLong();
-	}
+  public long getLong() {
+    return buffer.getLong();
+  }
 
-	public short getShort() {
-		return buffer.getShort();
-	}
+  public short getShort() {
+    return buffer.getShort();
+  }
 
-	public SeriaByteBuffer putByte(byte value) {
-		buffer.put(value);
+  public SeriaByteBuffer putByte(byte value) {
+    buffer.put(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putChar(char value) {
-		buffer.putChar(value);
+  public SeriaByteBuffer putChar(char value) {
+    buffer.putChar(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putDouble(double value) {
-		buffer.putDouble(value);
+  public SeriaByteBuffer putDouble(double value) {
+    buffer.putDouble(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putFloat(float value) {
-		buffer.putFloat(value);
+  public SeriaByteBuffer putFloat(float value) {
+    buffer.putFloat(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putInt(int value) {
-		buffer.putInt(value);
+  public SeriaByteBuffer putInt(int value) {
+    buffer.putInt(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putLong(long value) {
-		buffer.putLong(value);
+  public SeriaByteBuffer putLong(long value) {
+    buffer.putLong(value);
     return this;
-	}
+  }
 
-	public SeriaByteBuffer putShort(short value) {
-		buffer.putShort(value);
+  public SeriaByteBuffer putShort(short value) {
+    buffer.putShort(value);
     return this;
-	}
+  }
 }
