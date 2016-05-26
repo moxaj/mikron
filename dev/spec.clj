3290a0b89ba82e0cd51f333b1cd406ecb05241db
moxaj <viktor.magyari92@gmail.com>
2016-05-26 17:44:53 +0200

    improve specs

--- dev/spec.clj
+++ dev/spec.clj
@@ -3,8 +3,6 @@
             [mikron.type :as type]
             [mikron.util :as util]))
 
-(alter-var-root #'type/hierarchy type/add-custom-types [:body :fixture :coord :snapshot])
-
 (defmacro options-spec [& keys]
   `(spec/? (spec/and (spec/keys :opt-un ~(vec keys))
                      (spec/conformer (fn [value#]
@@ -16,16 +14,17 @@
   `(spec/and (spec/cat :schema keyword? ~@fields)
              (spec/conformer (juxt ~@(take-nth 2 fields)))))
 
-(spec/def ::pre
-  symbol?)
+(defn symbol-or-keyword? [form]
+  (or (symbol? form)
+      (keyword? form)))
 
-(spec/def ::post
-  symbol?)
+(defn any? [form]
+  true)
 
 (spec/def ::sorted-by
   (spec/and (spec/or :default #{:default}
                      :nil     nil?
-                     :symbol  symbol?)
+                     :symbol  symbol-or-keyword?)
             (spec/conformer second)))
 
 (spec/def ::constructor
@@ -37,16 +36,12 @@
                                  :default   :clojure.spec/invalid)
 
 (defmethod spec-type :simple [_]
-  (spec/spec (constantly true)))
+  any?)
 
 (defmethod spec-type :custom [_]
-  (spec/spec (constantly true)))
+  any?)
 
-(defmethod spec-type :list [_]
-  (composite-spec :options (options-spec)
-                  :schema  ::schema))
-
-(defmethod spec-type :vector [_]
+(defmethod spec-type :coll [_]
   (composite-spec :options (options-spec)
                   :schema  ::schema))
 
@@ -76,14 +71,14 @@
                   :values  (spec/coll-of keyword? [])))
 
 (defmethod spec-type :multi [_]
-  (composite-spec :options (options-spec)
-                  :selector symbol?
-                  :schemas  (spec/map-of (constantly true) ::schema)))
+  (composite-spec :options  (options-spec)
+                  :selector symbol-or-keyword?
+                  :schemas  (spec/map-of any? ::schema)))
 
 (defmethod spec-type :wrapped [_]
   (composite-spec :options (options-spec)
-                  :pre     symbol?
-                  :post    symbol?
+                  :pre     symbol-or-keyword?
+                  :post    symbol-or-keyword?
                   :schema  ::schema))
 
 (defmethod spec-type :clojure.spec/invalid [_]
@@ -93,27 +88,51 @@
   (spec/multi-spec spec-type util/type-of))
 
 (spec/def ::schemas
-  (spec/and (spec/map-of keyword? ::schema)
-            (fn [schemas]
-               (not-any? #(isa? type/hierarchy % :built-in)
-                         (keys schemas)))))
+  (spec/and (fn [schemas]
+              (if (some #(isa? type/initial-hierarchy % :built-in) schemas)
+                false
+                (do (alter-var-root #'type/hierarchy type/add-custom-types (keys schemas))
+                    true)))
+            (spec/map-of keyword? ::schema)))
+
+(spec/def ::schema-name
+  (spec/and ::schema keyword?))
+
+(spec/def ::buffer-size
+  (spec/and integer? pos?))
+
+(spec/def ::diff-routes
+  any?)
+
+(spec/def ::interp-routes
+  any?)
+
+(spec/def ::eq-ops
+  (spec/map-of ::schema-name symbol-or-keyword?))
 
 (spec/def ::options
-  (spec/keys :opt-un [::schemas]))
+  (spec/and (spec/keys :req-un [::schemas]
+                       :opt-un [::buffer-size
+                                ::diff-routes
+                                ::interp-routes
+                                ::eq-ops])
+            (spec/conformer (fn [{:keys [buffer-size] :or {buffer-size 10000} :as options}]
+                              (assoc options :buffer-size buffer-size)))))
+
+;; TODO  replace mikron.validate
+;; TODO  routes validation
+;; ISSUE conform does not flow into coll-of and map-of!
 
 ;; PLAYGROUND
 
-(spec/conform ::schemas
-  {:body     [:record {:user-data [:record {:id :int}]
-                       :position  :coord
-                       :angle     :float
-                       :body-type [:enum [:dynamic :static :kinetic]]
-                       :fixtures  [:list :fixture]}]
-   :fixture  [:record {:user-data [:record {:color :int}]
-                       :coords    [:list :coord]}]
-   :coord    [:tuple [:float :float]]
-   :snapshot [:record {:time   :long
-                       :bodies [:list :body]}]})
-
-;; ISSUES
-;; conform does not flow into coll-of / map-of
+(spec/conform ::options
+  {:schemas {:body     [:record {:user-data [:record {:id :int}]
+                                 :position  :coord
+                                 :angle     :float
+                                 :body-type [:enum [:dynamic :static :kinetic]]
+                                 :fixtures  [:list :fixture]}]
+             :fixture  [:record {:user-data [:record {:color :int}]
+                                 :coords    [:list :coord]}]
+             :coord    [:tuple [:float :float]]
+             :snapshot [:record {:time   :long
+                                 :bodies [:list :body]}]}})
