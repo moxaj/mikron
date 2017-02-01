9edaa18dfa161d81f4fa102f9d19f32521e51b3f
moxaj <viktor.magyari92@gmail.com>
2017-02-01 14:24:37 +0100

    Fix travis test script

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [ $PLATFORM == clj ] then
+if [ $PLATFORM == clj ]; then
   lein do clean, javac, test
 else
-  if [ $SELF_HOSTED == true ] then
-    if [ $TARGET == node ] then
+  if [ $SELF_HOSTED == true ]; then
+    if [ $TARGET == node ]; then
       lumo -vc `lein classpath` -k lumo_cache scripts/ci/lumo.clj
     fi # TODO add browser branch
   else
     lein do clean, javac, cljsbuild once $TARGET
-    if [ $TARGET == node ] then
+    if [ $TARGET == node ]; then
       node resources/test/node/app.js
     fi # TODO add browser branch
   fi
