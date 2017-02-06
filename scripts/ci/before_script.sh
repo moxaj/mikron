a56278f94825809d34a3288811ef23d95764ee18
moxaj <viktor.magyari92@gmail.com>
2017-02-06 16:55:20 +0100

    Fix before_script

--- scripts/ci/before_script.sh
+++ scripts/ci/before_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+npm install -g lumo-cljs
 sh -e /etc/init.d/xvfb start
 
 if [ ! -d slimerjs ]; then
   wget http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
   unzip slimerjs-0.10.2.zip
   mv slimerjs-0.10.2 ./slimerjs
-  npm install -g lumo-cljs
 fi
