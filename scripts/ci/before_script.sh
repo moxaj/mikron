09673962e0f4e8abcd00d5737fa3907773fafffd
moxaj <viktor.magyari92@gmail.com>
2017-02-06 17:10:48 +0100

    Fix before_script

--- scripts/ci/before_script.sh
+++ scripts/ci/before_script.sh
@@ -3,8 +3,8 @@
 npm install -g lumo-cljs
 sh -e /etc/init.d/xvfb start
 
-if [ ! -d slimerjs ]; then
+if [ ! -d $TRAVIS_BUILD_DIR/slimerjs ]; then
   wget http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
   unzip slimerjs-0.10.2.zip
-  mv slimerjs-0.10.2 ./slimerjs
+  mv slimerjs-0.10.2 $TRAVIS_BUILD_DIR/slimerjs
 fi
