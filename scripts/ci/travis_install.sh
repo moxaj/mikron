e63ae8fb483a5d89461d6074526a7665e0397802
moxaj <viktor.magyari92@gmail.com>
2017-02-06 18:21:05 +0100

    Fix install script

--- scripts/ci/travis_install.sh
+++ scripts/ci/travis_install.sh
@@ -2,7 +2,7 @@
 
 CACHE_DIR=$TRAVIS_BUILD_DIR/dep_cache
 
-if [ ! -d $CACHE_DIR/lumo]; then
+if [ ! -d $CACHE_DIR/lumo ]; then
   wget https://github.com/anmonteiro/lumo/releases/download/1.1.0/lumo_linux64.zip
   unzip lumo_linux64.zip
   mv lumo $CACHE_DIR/lumo/lumo
@@ -10,7 +10,7 @@ fi
 
 PATH=$CACHE_DIR/lumo:$PATH
 
-if [ ! -d $CACHE_DIR/slimerjs]; then
+if [ ! -d $CACHE_DIR/slimerjs ]; then
   wget http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
   unzip slimerjs-0.10.2.zip
   mv slimerjs-0.10.2 $CACHE_DIR/slimerjs
