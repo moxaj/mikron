3c74ef15c9c9c151518e856b0bf03551c4157e10
moxaj <viktor.magyari92@gmail.com>
2017-02-06 16:21:14 +0100

    Fix slimerjs install

---
+++ scripts/ci/before_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sh -e /etc/init.d/xvfb start
+
+if [ ! -d slimerjs ]; then
+  wget http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
+  unzip slimerjs-0.10.2.zip
+  mv slimerjs-0.10.2 ./slimerjs
+  npm install -g lumo-cljs
+fi
