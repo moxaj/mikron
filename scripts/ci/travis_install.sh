865d200dcb5e41d2a6a5dfcacb22cb5a9ebc36ae
moxaj <viktor.magyari92@gmail.com>
2017-02-06 18:45:08 +0100

    Export PATH correctly

--- scripts/ci/travis_install.sh
+++ scripts/ci/travis_install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CACHE_DIR=$TRAVIS_BUILD_DIR/dep_cache
-
 if [ ! -d $CACHE_DIR/lumo ]; then
   wget https://github.com/anmonteiro/lumo/releases/download/1.1.0/lumo_linux64.zip
   unzip lumo_linux64.zip
@@ -10,15 +8,11 @@ if [ ! -d $CACHE_DIR/lumo ]; then
   mv lumo $CACHE_DIR/lumo/lumo
 fi
 
-PATH=$CACHE_DIR/lumo:$PATH
-
 if [ ! -d $CACHE_DIR/slimerjs ]; then
   wget http://download.slimerjs.org/releases/0.10.2/slimerjs-0.10.2.zip
   unzip slimerjs-0.10.2.zip
   mv slimerjs-0.10.2 $CACHE_DIR/slimerjs
 fi
-
-PATH=$CACHE_DIR/slimerjs:$PATH
 
 if [ ! -d $CACHE_DIR/boot ]; then
   wget https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh
@@ -27,5 +21,3 @@ if [ ! -d $CACHE_DIR/boot ]; then
   mkdir -p $CACHE_DIR/boot
   mv boot $CACHE_DIR/boot/boot boot
 fi
-
-PATH=$CACHE_DIR/boot:$PATH
