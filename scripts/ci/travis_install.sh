d23f8b2417be2e6c23ee36c1a654d9e3a46ec678
moxaj <viktor.magyari92@gmail.com>
2017-02-07 15:07:58 +0100

    Update travis config

--- scripts/ci/travis_install.sh
+++ scripts/ci/travis_install.sh
@@ -13,11 +13,3 @@ if [ ! -d $CACHE_DIR/slimerjs ]; then
   unzip slimerjs-0.10.2.zip
   mv slimerjs-0.10.2 $CACHE_DIR/slimerjs
 fi
-
-if [ ! -d $CACHE_DIR/boot ]; then
-  wget https://github.com/boot-clj/boot-bin/releases/download/latest/boot.sh
-  mv boot.sh boot
-  chmod a+x boot
-  mkdir -p $CACHE_DIR/boot
-  mv boot $CACHE_DIR/boot/boot
-fi
