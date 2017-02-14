8f0a51640b8d7b25f49fc1787086feab45724b39
moxaj <viktor.magyari92@gmail.com>
2017-02-14 17:45:28 +0100

    Add generate-cljs-cache lumo script [skip ci]

--- scripts/lumo/generate_cljs_cache.cljs
+++ scripts/lumo/generate_cljs_cache.cljs
@@ -1,11 +1,7 @@
 (ns generate-cljs-cache
   (:require [clojure.string :as string]))
 
-(def nexeres (js/require "nexeres"))
-
-(def mkdirp (js/require "mkdirp"))
-
-(doseq [resource (filter #(re-matches #"cljs.*" %) (.keys nexeres))]
-  (let [filename (str "docs\\cache-cljs\\" (string/replace resource #"[/\\]" "_SLASH_"))]
-    (mkdirp (js/path.dirname filename)
-            #(js/fs.writeFile filename (js/zlib.inflateSync (.get nexeres resource)) (constantly true)))))
+(let [nexeres (js/require "nexeres")]
+  (doseq [resource (filter #(re-matches #"cljs.*" %) (.keys nexeres))]
+    (let [filename (str "docs/cache-cljs/" (string/replace resource #"[/\\]" "_SLASH_"))]
+      (js/fs.writeFile filename (js/zlib.inflateSync (.get nexeres resource)) (constantly true)))))
